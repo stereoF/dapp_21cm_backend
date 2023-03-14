73bc9f11cda053f166ea6c94ef98bc049445f816
zhangjingbo <172886566@163.com>
2023-03-14 09:53:57 +0800

    连接数据库，读取用户信息

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+from sqlalchemy.orm import Session
 
+from sql_app import schemas, models, crud
+from sql_app.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 origins = [
@@ -16,9 +22,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/users/{user_addr}", response_model=schemas.User)
+def user_infos(user_addr: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_addr=user_addr)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
 
 @app.post("/uploadfile/")
