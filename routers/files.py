3539b078e6f02767a4d3f9986396078076cd4573
stereoF <firmamentsess@gmail.com>
2023-03-17 00:40:11 +0800

    get cid when received files

--- routers/files.py
+++ routers/files.py
@@ -1,4 +1,5 @@
 from fastapi import UploadFile, APIRouter
+from service.ipfs import add_files
 
 
 router = APIRouter(
@@ -6,12 +7,22 @@ router = APIRouter(
     tags=["files"]
 )
 
-@router.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    print(file.filename)
-    return {"filename": file.filename}
+
+# @router.post("/uploadfile/")
+# async def create_upload_file(file: UploadFile):
+#     file_dict = {
+#         file.filename: file.file
+#     }
+
+#     return {"filename": file.filename}
 
 
 @router.post("/uploadfiles/")
 async def create_upload_files(files: list[UploadFile]):
-    return {"filenames": [file.filename for file in files]}
+    files_dict: dict = {}
+    for f in files:
+        files_dict[f.filename] = f.file
+    # file_list = [{'path': f.filename, 'content': f.file} for f in files]
+    df = add_files(files_dict)
+    cid = df.loc[df['Name'] == '', 'Hash'].values[0]
+    return {"cid": cid}
