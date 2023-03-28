2672d724683e29b80d0c355277c05000eaa0713a
stereoF <firmamentsess@gmail.com>
2023-03-28 21:45:36 +0800

    create tables from orm

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from urllib import parse
-host = 'bj-cynosdbmysql-grp-6m11pba6.sql.tencentcdb.com'
-port = '26045'
-username = 'desci_test_user'
+host = '127.0.0.1'
+port = '3306'
+username = 'root'
 password = parse.quote_plus('DeSci@21cm_testdb')
-db = 'desci_testdb'
+db = 'test'
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset=utf8".format(
     username=username, password=password, host=host, port=port, db=db)
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
--- sql_app/models.py
+++ sql_app/models.py
@@ -6,21 +6,21 @@ from .database import Base
 class User(Base):
     __tablename__ = "user_info"
 
-    addr = Column(String, primary_key=True, index=False)
-    uname = Column(String)
-    unit = Column(String)
-    email = Column(String)
+    addr = Column(String(80), primary_key=True, index=False)
+    uname = Column(String(80))
+    unit = Column(String(100))
+    email = Column(String(80))
     admin = relationship("Admin", back_populates="user")
 
 
 class Journal(Base):
     __tablename__ = "journal_infos"
 
-    address = Column(String, primary_key=True, index=True)
-    jname = Column(String, index=False)
-    first_cate = Column(String)
-    sec_cate = Column(String)
-    third_cate = Column(String)
+    address = Column(String(80), primary_key=True, index=True)
+    jname = Column(String(100), index=False)
+    first_cate = Column(String(100))
+    sec_cate = Column(String(100))
+    third_cate = Column(String(100))
     subject_index = Column(Integer, autoincrement=True)
     remark = Column(TEXT)
     follow = relationship("Follow", back_populates="journal")
@@ -29,31 +29,35 @@ class Journal(Base):
 class Follow(Base):
     __tablename__ = "journal_follow"
 
-    user_addr = Column(String, primary_key=True)
-    journal_addr = Column(String, ForeignKey("journal_infos.address"), primary_key=True)
+    user_addr = Column(String(80), primary_key=True)
+    journal_addr = Column(String(80), ForeignKey("journal_infos.address"), primary_key=True)
     journal = relationship("Journal", back_populates="follow")
 
 
 class Admin(Base):
     __tablename__ = "journal_admins"
 
-    user_addr = Column(String, ForeignKey("user_info.addr"), primary_key=True)
+    user_addr = Column(String(80), ForeignKey("user_info.addr"), primary_key=True)
     type = Column(Integer)
-    journal_addr = Column(String, ForeignKey("journal_infos.address"), primary_key=True)
+    journal_addr = Column(String(80), ForeignKey("journal_infos.address"), primary_key=True)
     user = relationship("User", back_populates="admin")
 
 
 class Article(Base):
     __tablename__ = "publication_infos"
 
-    cid = Column(String, primary_key=True)
-    author_addr = Column(String)
+    cid = Column(String(80), primary_key=True)
+    author_addr = Column(String(80))
     c_status = Column(Integer)
     descs = Column(TEXT)
-    title = Column(String)
-    author_info = Column(String)
+    title = Column(String(200))
+    author_info = Column(String(200))
     abstract = Column(TEXT)
     submit_time = Column(DATETIME)
-    prev_cid = Column(String)
-    next_cid = Column(String)
-    journal_addr = Column(String, ForeignKey("journal_infos.address"))
+    prev_cid = Column(String(80))
+    next_cid = Column(String(80))
+    journal_addr = Column(String(80), ForeignKey("journal_infos.address"))
+
+
+
+# Base.metadata.create_all(engine)
